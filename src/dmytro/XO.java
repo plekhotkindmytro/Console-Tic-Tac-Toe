3f51ac9b394cb0b3eaf06549eb3cdcd1abf16969
Dmytro_Plekhotkin <dmytro_plekhotkin@epam.com>
2016-03-30 15:27:43 +0300

    Add Normal Computer that plays defencive style.

--- src/dmytro/XO.java
+++ src/dmytro/XO.java
@@ -42,7 +42,7 @@ public class XO {
                 userInputs.add(userInput);
                 deck = deck.replace(userInput, "X");
                 placeholders.remove(userInput);
-                computerTurn();
+                computerTurnNormal();
             } else {
                 String errorMessage = "Your input is not valid. Try one more time.";
                 System.out.println(errorMessage);
@@ -52,7 +52,7 @@ public class XO {
 
             boolean userWon = isWinner(userInputs);
             boolean computerWon = isWinner(computerInputs);
-            if (userWon && computerWon) {
+            if ((userWon && computerWon) || (!userWon && !computerWon && placeholders.size() == 0)) {
                 System.out.println("Draw!!! No winners!");
                 break;
             } else if (userWon) {
@@ -76,7 +76,6 @@ public class XO {
         List<String> winResult8 = Arrays.asList("3", "5", "7");
         List<List<String>> winResultsAll = Arrays.asList(winResult1, winResult2, winResult3, winResult4, winResult5,
                 winResult6, winResult7, winResult8);
-
         boolean isWinner = false;
         for (List<String> winResult : winResultsAll) {
             if (inputs.containsAll(winResult)) {
@@ -89,13 +88,98 @@ public class XO {
 
     private Random random = new Random();
 
-    private void computerTurn() {
-        String computerInput = placeholders.get(random.nextInt(placeholders.size()));
+    private void computerTurnEasy() {
+        if (placeholders.size() > 0) {
+            String computerInput = placeholders.get(random.nextInt(placeholders.size()));
+            computerInputs.add(computerInput);
+            deck = deck.replace(computerInput, "O");
+            placeholders.remove(computerInput);
+        }
 
-        computerInputs.add(computerInput);
-        deck = deck.replace(computerInput, "O");
-        placeholders.remove(computerInput);
+    }
 
+    private void computerTurnNormal() {
+        List<String> winResult1 = new ArrayList<String>(Arrays.asList("1", "2", "3"));
+        List<String> winResult2 = new ArrayList<String>(Arrays.asList("4", "5", "6"));
+        List<String> winResult3 = new ArrayList<String>(Arrays.asList("7", "8", "9"));
+        List<String> winResult4 = new ArrayList<String>(Arrays.asList("1", "4", "7"));
+        List<String> winResult5 = new ArrayList<String>(Arrays.asList("2", "5", "8"));
+        List<String> winResult6 = new ArrayList<String>(Arrays.asList("3", "6", "9"));
+        List<String> winResult7 = new ArrayList<String>(Arrays.asList("1", "5", "9"));
+        List<String> winResult8 = new ArrayList<String>(Arrays.asList("3", "5", "7"));
+        List<List<String>> winResultsAll = new ArrayList<List<String>>(Arrays.asList(winResult1, winResult2,
+                winResult3, winResult4, winResult5, winResult6, winResult7, winResult8));
+
+        List<List<String>> dangerList = new ArrayList<List<String>>();
+        for (List<String> winResult : winResultsAll) {
+            boolean hasComputerElement = false;
+            for (String computerInput : computerInputs) {
+                if (winResult.contains(computerInput)) {
+                    hasComputerElement = true;
+                    break;
+                }
+            }
+            if (!hasComputerElement) {
+                int userElementsCount = 0;
+                for (String userInput : userInputs) {
+                    if (winResult.contains(userInput)) {
+                        userElementsCount++;
+                    }
+                }
+                if (userElementsCount == 2) {
+                    dangerList.add(winResult);
+                }
+            }
+        }
+
+        if (dangerList.size() > 0) {
+            List<String> defensiveTurnList = dangerList.get(0);
+            defensiveTurnList.removeAll(userInputs);
+            String defenciveTurn = defensiveTurnList.get(0);
+
+            deck = deck.replace(defenciveTurn, "O");
+            placeholders.remove(defenciveTurn);
+            computerInputs.add(defenciveTurn);
+        } else {
+            List<List<String>> goalList = new ArrayList<List<String>>();
+            for (List<String> winResult : winResultsAll) {
+                boolean hasUserElement = false;
+                for (String userInput : userInputs) {
+                    if (winResult.contains(userInput)) {
+                        hasUserElement = true;
+                        break;
+                    }
+                }
+                if (!hasUserElement) {
+                    int computerElementsCount = 0;
+                    for (String computerInput : computerInputs) {
+                        if (winResult.contains(computerInput)) {
+                            computerElementsCount++;
+                        }
+                    }
+                    if (computerElementsCount >= 1) {
+                        goalList.add(winResult);
+                    }
+                }
+            }
+
+            if (goalList.size() > 0) {
+                List<String> offenciveTurnList = goalList.get(0);
+                offenciveTurnList.removeAll(computerInputs);
+                String offenciveTurn = offenciveTurnList.get(0);
+
+                deck = deck.replace(offenciveTurn, "O");
+                placeholders.remove(offenciveTurn);
+                computerInputs.add(offenciveTurn);
+            } else {
+                if (placeholders.size() > 0) {
+                    String computerInput = placeholders.get(random.nextInt(placeholders.size()));
+                    deck = deck.replace(computerInput, "O");
+                    placeholders.remove(computerInput);
+                    computerInputs.add(computerInput);
+                }
+            }
+        }
     }
 
     private boolean valid(String userInput) {
